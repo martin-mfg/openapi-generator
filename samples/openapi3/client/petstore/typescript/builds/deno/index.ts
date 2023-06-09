c764af9a13fd92d4770bcd0c438701a80e8c048b
Martin <2026226+martin-mfg@users.noreply.github.com>
2023-06-09 20:18:10 +0200

    typescript fixes: use model package only when needed

--- samples/openapi3/client/petstore/typescript/builds/deno/index.ts
+++ samples/openapi3/client/petstore/typescript/builds/deno/index.ts
@@ -1,6 +1,5 @@
 export * from "./http/http.ts";
 export * from "./auth/auth.ts";
-export * from "./models/all.ts";
 export { createConfiguration } from "./configuration.ts"
 export type { Configuration } from "./configuration.ts"
 export * from "./apis/exception.ts";
