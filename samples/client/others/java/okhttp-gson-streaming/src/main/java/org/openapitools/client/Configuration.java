7818348b267af1743f0047b91b4ae32a4de95533
Martin <2026226+martin-mfg@users.noreply.github.com>
2023-06-11 18:13:31 +0200

    apply build fixes

--- samples/client/others/java/okhttp-gson-streaming/src/main/java/org/openapitools/client/Configuration.java
+++ samples/client/others/java/okhttp-gson-streaming/src/main/java/org/openapitools/client/Configuration.java
@@ -15,7 +15,7 @@ package org.openapitools.client;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.2";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
