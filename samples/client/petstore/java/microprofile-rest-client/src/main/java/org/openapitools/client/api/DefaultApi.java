eb83836a7cb3b6a7ac8571b941e2aaf1c539e3d3
Martin <2026226+martin-mfg@users.noreply.github.com>
2023-06-12 17:10:21 +0200

    fix /microprofile-rest-client build

--- samples/client/petstore/java/microprofile-rest-client/src/main/java/org/openapitools/client/api/DefaultApi.java
+++ samples/client/petstore/java/microprofile-rest-client/src/main/java/org/openapitools/client/api/DefaultApi.java
@@ -42,6 +42,6 @@ public interface DefaultApi  {
 
     @GET
     
-    @Produces({ "/" })
+    @Produces({ "*/*" })
     ExampleResponse retrieveSomething() throws ApiException, ProcessingException;
 }
